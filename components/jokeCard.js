7d52bdc0d5cc9324a45489d658e51818fe2566fc
Tion Blackmon <tionblackmon@icloud.com>
2023-11-21 08:36:44 -0600

    Snapshot - made promises.js

--- components/jokeCard.js
+++ components/jokeCard.js
@@ -4,9 +4,8 @@ const jokeCard = () => {
   const domString = `
   <div class="card" style="width: 18rem;">
     <h1>Joke Generator</h1>
-    <small>Open your dev tools</small><br />
+    <small>You are going to laugh!</small><br />
     <button class="btn btn-danger" id="click-me">Get a Joke</button><br />
-    <hr />
   </div>`;
 
   renderToDOM('#app', domString);
